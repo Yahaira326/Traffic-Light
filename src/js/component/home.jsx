e1336ba0a2d2e1b26e9931706e81202f02ce13b3
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-11-26 07:38:58 -0600

    commit 1

---
+++ src/js/component/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Light from "./light.jsx";
+
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+	return (
+		<div>
+			<Light/>
+		</div>
+	);
+};
+
+export default Home;
